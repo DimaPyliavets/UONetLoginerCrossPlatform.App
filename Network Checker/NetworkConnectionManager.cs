205b83cbdffac364ecd4f2e2d9500a1b90239ab8
Dmytro Pyliavets <demitriy26081999@gmail.com>
2023-07-07 16:31:50 +0200

    backendv1 Add project files.

---
+++ Network Checker/NetworkConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace UONetLoginerCrossPlatform.Network_Checker
+{
+    public class NetworkConnectionManager
+    {
+        private INetworkConnection connection;
+
+        public NetworkConnectionManager(INetworkConnection connect)
+        {
+            connection = connect;
+        }
+
+        public async Task<bool> IsConnectedToNetwork()
+        {
+            return await connection.IsConnectedToNetwork();
+        }
+    }
+}
